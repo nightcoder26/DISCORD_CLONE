1da99f2b4dd27668a993e0cabe4b205bcf4f1217
Knight <77844703+nightcoder26@users.noreply.github.com>
2024-06-05 21:36:49 +0530

    Add authentication to app component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ServerContent from "./myComponents/ServerContent";
 import ServerInfo from "./myComponents/ServerInfo";
 import ServerName from "./myComponents/ServerName";
 import "./App.css";
+import { useSelector } from "react-redux";
+import LoginSignup from "./myComponents/LoginSignup";
+import { Link } from "react-router-dom";
+import ErrorComp from "./myComponents/ErrorComp";
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+  const username = useSelector((state) => state.servers.loggedUser);
+  useEffect(() => {
+    if (username !== "SamayRaina") {
+      setAuthenticated(true);
+    } else {
+      // thik h na
+      setAuthenticated(false);
+    }
+  }, [authenticated]);
+
   return (
     <>
-      <div className="main">
-        <ServerName />
-        <ServerInfo />
-        <ServerContent />
-      </div>
+      {authenticated ? (
+        <div className="main">
+          <ServerName />
+          <ServerInfo />
+          <ServerContent />
+        </div>
+      ) : (
+        <ErrorComp />
+      )}
     </>
   );
   //banado
